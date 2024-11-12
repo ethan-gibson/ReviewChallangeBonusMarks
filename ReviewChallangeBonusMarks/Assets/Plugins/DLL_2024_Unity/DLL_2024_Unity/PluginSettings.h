9444beb576a8c22c121dc74cda47ddf69b41f902
Lvancyho <ivanhocy@gmail.com>
2024-11-12 11:13:30 -0500

    my part

---
+++ ReviewChallangeBonusMarks/Assets/Plugins/DLL_2024_Unity/DLL_2024_Unity/PluginSettings.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef _PLUGIN_SETTINGS_
+#define _PLUGIN_SETTINGS_
+
+	#ifdef PLUGIN_EXPORTS
+	#define PLUGIN_API __declspec(dllexport)
+	#elif PLUGIN_IMPORTS
+	#define PLUGIN_API __declspec(dllimport)
+	#else
+	#define PLUGIN_API
+	#endif
+
+#endif /*defined (_PLUGIN_SETTINGS_)*/
